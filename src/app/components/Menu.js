2412f0be9e02e9ad5c573ffa593287f719b14eb9
mikaellybarth <wwwmikasouza06@gmail.com>
2024-02-21 13:31:16 -0400

    Commit teste

---
+++ src/app/components/Menu.js
@@ -0,0 +1,13 @@
+export default function Menu(){
+    return(
+        <header>
+            <h1>Menu</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Registrar</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
