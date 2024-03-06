1d174a5140d92cc9ab8ee379dc8b3182499d7183
mikaellybarth <wwwmikasouza06@gmail.com>
2024-03-06 13:58:15 -0400

    commit

--- src/app/components/Map.js
+++ src/app/components/Map.js
@@ -1,14 +1,14 @@
-import styles from "./map.module.css"
 
 export default function Map(){
     return(
-
-        <iframe className={styles.mapa}
+    <center>
+    <iframe 
         id="gmap_canvas"
         src={"https://maps.google.com/maps?       q=Nova%20Andradina%20+IFMS+&t=&z=13&ie=UTF8&iwloc=&output=embed"}
         frameborder="0" scrolling="no"
         marginheight="0"
         marginwidth="0">
-</iframe>
+    </iframe>
+    </center>
     );
 };
